841a5fd98f8c341e9834604946cb3380cdc892c3
Sergey Naumov <svcomplex@gmail.com>
2023-12-12 13:42:32 +0300

    text subtype email

---
+++ src/components/fields/text_subtypes/email.vue
@@ -0,0 +1,30 @@
+<template>
+<span>
+
+    <span v-for="(e,idx) in splitted" :key="`email-${idx}`" style="padding-left: 3px;">
+        <a :href="'mailto:'+e" >{{ e }}</a>
+        <template v-if="idx!=splitted.length-1"> | </template>
+    </span>
+</span>
+</template>
+<script>
+export default {
+    props:['field','value','for_slide'],
+    created(){
+
+
+    },
+    computed:{
+        splitted(){
+            let splitted=[]
+            for(let p of this.value.split(/,\s*/)){
+                if(/@/.test(p)){
+                    splitted.push(p)
+                }
+            }
+            return splitted
+        }
+    }
+
+}
+</script>
